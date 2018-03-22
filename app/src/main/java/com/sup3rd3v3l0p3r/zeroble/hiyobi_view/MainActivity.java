2546135012f7a3482bbb86562a320f1c7db71439
Zeroble <dlalsdyd0219@gmail.com>
2018-03-22 10:31:55 +0900

    asdf

--- app/src/main/java/com/sup3rd3v3l0p3r/zeroble/hiyobi_view/MainActivity.java
+++ app/src/main/java/com/sup3rd3v3l0p3r/zeroble/hiyobi_view/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -26,40 +29,50 @@ public class MainActivity extends AppCompatActivity {
     Elements img;
     Document doc = null;
     HiyobiListAdapter adapter;
-    ListView listview ;
+    ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listview =  findViewById(R.id.hiyobi_list);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        listview = findViewById(R.id.hiyobi_list);
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                HiyobiListItem item = (HiyobiListItem) parent.getItemAtPosition(position) ;
+                HiyobiListItem item = (HiyobiListItem) parent.getItemAtPosition(position);
 
-                Toast.makeText(getApplicationContext(),item.getUrlStr() ,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), item.getUrlStr(), Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(getApplicationContext(), Viewer.class);
-                intent.putExtra("URL",item.getUrlStr());
+                intent.putExtra("URL", item.getUrlStr());
                 startActivity(intent);
             }
         });
 
         adapter = new HiyobiListAdapter();
 
-
         new LoadListPage(1).execute();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     public class LoadListPage extends AsyncTask {
         int page;
-        public LoadListPage(int page){
+
+        public LoadListPage(int page) {
             this.page = page;
         }
+
         @Override
         protected Object doInBackground(Object[] objects) {
             try {
-                doc = Jsoup.connect("https://hiyobi.me/list/"+page).get();
+                doc = Jsoup.connect("https://hiyobi.me/list/" + page).get();
                 title = doc.select("div.gallery-content > span > h5 > a > b");//aaa
                 artist = doc.select("div.gallery-content > span > table > tbody > tr:nth-child(1) > td:nth-child(2)");
                 original = doc.select("div.gallery-content > span > table > tbody > tr:nth-child(2) > td:nth-child(2)");
@@ -77,15 +90,15 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(Object o) {
             super.onPostExecute(o);
             int cnt = 0;
-            Toast.makeText(MainActivity.this,"Loading ok",Toast.LENGTH_SHORT).show();
-            for(Element element: title) {
+            Toast.makeText(MainActivity.this, "Loading ok", Toast.LENGTH_SHORT).show();
+            for (Element element : title) {
                 adapter.addItem(
                         img.get(cnt).attr("src"),
-                        "제목 : "+element.text(),
-                        "작가 : "+artist.get(cnt).text(),
-                        "시리즈 : "+original.get(cnt).text(),
-                        "종류 : "+type.get(cnt).text(),
-                        tags.get(cnt).text().replace(" ♀","♀ ").replace(" ♂","♂ "),
+                        "제목 : " + element.text(),
+                        "작가 : " + artist.get(cnt).text(),
+                        "시리즈 : " + original.get(cnt).text(),
+                        "종류 : " + type.get(cnt).text(),
+                        tags.get(cnt).text().replace(" ♀", "♀ ").replace(" ♂", "♂ "),
                         url.get(cnt).attr("href"));
                 cnt++;
             }
