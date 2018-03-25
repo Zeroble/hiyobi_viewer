21bf1adb5d84b0b53a8592d3c914be4192d605ce
Zeroble <dlalsdyd0219@gmail.com>
2018-03-26 01:15:52 +0900

    adsf

--- app/src/main/java/com/sup3rd3v3l0p3r/zeroble/hiyobi_view/ImageFragment.java
+++ app/src/main/java/com/sup3rd3v3l0p3r/zeroble/hiyobi_view/ImageFragment.java
@@ -1,18 +1,33 @@
 package com.sup3rd3v3l0p3r.zeroble.hiyobi_view;
 
-import android.app.Fragment;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by dlals on 2018-03-25.
  */
 
-public class ImageFragment extends Fragment {
+public class ImageFragment extends android.support.v4.app.Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.image_fragment, container, false);
+
+        LinearLayout linearLayout=(LinearLayout)inflater.inflate(R.layout.page,container,false);
+
+        LinearLayout background=(LinearLayout)linearLayout.findViewById(R.id.background);
+        TextView page_num=(TextView)linearLayout.findViewById(R.id.page_num);
+        page_num.setText(String.valueOf(1));
+        background.setBackground(new ColorDrawable(0xff6dc6d2));
+
+        return linearLayout;
     }
 }
--- app/src/main/java/com/sup3rd3v3l0p3r/zeroble/hiyobi_view/Viewer.java
+++ app/src/main/java/com/sup3rd3v3l0p3r/zeroble/hiyobi_view/Viewer.java
@@ -3,6 +3,10 @@ package com.sup3rd3v3l0p3r.zeroble.hiyobi_view;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -28,10 +32,46 @@ public class Viewer extends AppCompatActivity {
     ImageView loadingImg;
     int cnt = 0;
     int indexSize;
+    int MAX_PAGE=3;                         //View Pager의 총 페이지 갯수를 나타내는 변수 선언
+    Fragment cur_fragment=new Fragment();   //현재 Viewpager가 가리키는 Fragment를 받을 변수 선언
+
+    private class adapter extends FragmentPagerAdapter {                    //adapter클래스
+        public adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if(position<0 || MAX_PAGE<=position)        //가리키는 페이지가 0 이하거나 MAX_PAGE보다 많을 시 null로 리턴
+                return null;
+            switch (position){              //포지션에 맞는 Fragment찾아서 cur_fragment변수에 대입
+                case 0:
+                    cur_fragment=new ImageFragment();
+                    break;
+
+                case 1:
+                    cur_fragment=new page_2();
+                    break;
+
+                case 2:
+                    cur_fragment=new page_3();
+                    break;
+            }
+
+            return cur_fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return MAX_PAGE;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viewer);
+
+
 
         loadingImg = findViewById(R.id.viewer_loading);
         Glide.with(this).load(R.drawable.giphy).into(new GlideDrawableImageViewTarget(loadingImg));
@@ -52,6 +92,10 @@ public class Viewer extends AppCompatActivity {
                     Glide.with(getApplicationContext()).load(images.get(cnt++).text()).override(540,960).into(imageView);
             }
         });
+
+        ViewPager viewPager=(ViewPager)findViewById(R.id.viewPager);        //Viewpager 선언 및 초기화
+        viewPager.setAdapter(new adapter(getSupportFragmentManager()));     //선언한 viewpager에 adapter를 연결
+
     }
 
     public class Loading extends AsyncTask {
